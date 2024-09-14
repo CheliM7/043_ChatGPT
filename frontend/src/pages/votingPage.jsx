e8e033e691a87f14b240399fa40f719775caed6c
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-09-14 13:25:15 +0530

    added conditional rendering to the voting page

--- frontend/src/pages/votingPage.jsx
+++ frontend/src/pages/votingPage.jsx
@@ -1,7 +1,6 @@
-// VotingPage.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import CandidateTile from '../components/votingPage/CandidateTile';
-import PublicPolls from '../components/votingPage/PublicPolls';
+import GraphComponent from '../components/votingPage/GraphComponent';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -18,12 +17,6 @@ const Heading = styled.h1`
   color: #333;
 `;
 
-const SubHeading = styled.h2`
-  font-size: 20px;
-  margin: 20px 0;
-  color: #555;
-`;
-
 const TilesContainer = styled.div`
   display: flex;
   justify-content: space-around;
@@ -33,6 +26,13 @@ const TilesContainer = styled.div`
 `;
 
 const VotingPage = () => {
+  const [voteSuccessful, setVoteSuccessful] = useState(false);
+
+  const handleVote = () => {
+    console.log("Vote button clicked"); // Log when button is clicked
+    setVoteSuccessful(true);
+  };
+
   // Sample candidates data
   const candidates = [
     { name: 'Sajith Premadasa', image: 'path/to/image1.jpg' },
@@ -43,16 +43,22 @@ const VotingPage = () => {
 
   return (
     <Container>
-      <Heading>If you had the chance to vote now, who would you vote for?</Heading>
-      <TilesContainer>
-        {candidates.map((candidate, index) => (
-          <CandidateTile 
-            key={index}
-            name={candidate.name}
-            image={candidate.image}
-          />
-        ))}
-      </TilesContainer>
+      <Heading>
+        {voteSuccessful ? 'Current voting Results' : 'If you had the chance to vote now, who would you vote for?'}
+      </Heading>
+      {!voteSuccessful && (
+        <TilesContainer>
+          {candidates.map((candidate, index) => (
+            <CandidateTile
+              key={index}
+              name={candidate.name}
+              image={candidate.image}
+              onVote={handleVote} // Ensure this is passed correctly
+            />
+          ))}
+        </TilesContainer>
+      )}
+      {voteSuccessful && <GraphComponent />} {/* Render graph component conditionally */}
     </Container>
   );
 };
